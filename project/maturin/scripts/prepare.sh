f42cf6d899f6e9330f3ca05c75e5651c9e05feb3
吴小白 <296015668@qq.com>
2025-01-16 16:59:36 +0800

    feat: 更新模板

--- project/maturin/scripts/prepare.sh
+++ project/maturin/scripts/prepare.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-commit=bd33565a45aad30903eed9d825c82770d94fb639
+commit=c81f1f76679afa29d1a9e4da56390f420b4d61c1
 wget -O - "https://github.com/loong64/maturin/commit/${commit}.patch" | patch -p1
